c80ed6b8e2d8c59e3822eab45d4d48f2cf83d6cd
Darren Hale <djhale.development@gmail.com>
2023-04-04 13:47:32 +0100

    Utilizr.Logging no longer depends on Utilizr.Extensions, can now target netstandard2.1.

--- Utilizr.Logging/Formatters/Formatter.cs
+++ Utilizr.Logging/Formatters/Formatter.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Utilizr.Extensions;
+//using Utilizr.Extensions;
 using Utilizr.Logging.Interfaces;
 
 namespace Utilizr.Logging.Formatters
