8c5bf9526cc2f203cd557af35002532be956dd61
Darren Hale <djhale.development@gmail.com>
2023-06-02 14:28:09 +0100

    Added more controls to Utilizr.WPF: InlineText, ClickableBorder, ClickableRun, CapsLockHelper, Validater, PerMonitorDPIWindow.

---
+++ Utilizr.WPF/PerMonitorDPIWindow.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using Utilizr.Logging;
+
+namespace Utilzr.WPF
+{
+
+    public class PerMonitorDPIWindow: Window
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;        // x position of upper-left corner
+            public int Top;         // y position of upper-left corner
+            public int Right;       // x position of lower-right corner
+            public int Bottom;      // y position of lower-right corner
+        }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr MonitorFromWindow(IntPtr hwnd, uint dwFlags);
+        const int MONITOR_DEFAULTTONULL = 0;
+        const int MONITOR_DEFAULTTOPRIMARY = 1;
+        const int MONITOR_DEFAULTTONEAREST = 2;
+
+        [DllImport("shcore.dll", CallingConvention = CallingConvention.StdCall)]
+        protected static extern int GetDpiForMonitor(IntPtr hMonitor, int dpiType, ref uint xDpi, ref uint yDpi);
+
+        [DllImport("shcore.dll", CallingConvention = CallingConvention.StdCall)]
+        protected static extern int GetProcessDpiAwareness(IntPtr handle, ref ProcessDpiAwareness awareness);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+
+        public const int SWP_NOZORDER = 0x0004;
+        public const int SWP_NOOWNERZORDER = 0x0200;
+        public const int SWP_NOACTIVATE = 0x0010;
+
+
+        public enum ProcessDpiAwareness
+        {
+            Process_DPI_Unaware = 0,
+            Process_System_DPI_Aware = 1,
+            Process_Per_Monitor_DPI_Aware = 2
+        }
+
+        protected enum MonitorDpiType
+        {
+            MDT_Effective_DPI = 0,
+            MDT_Angular_DPI = 1,
+            MDT_Raw_DPI = 2,
+            MDT_Default = MDT_Effective_DPI
+        }
+
+
+        const int WM_DPICHANGED = 0x02E0;
+
+
+
+        private Point _currentDpi;
+        private double _wpfDpi;
+        private HwndSource _hwndSource;
+        private double _scaleFactor;
+        private bool _perMonitorDpiAware;
+
+        public PerMonitorDPIWindow()
+        {
+            Loaded += OnLoaded;
+            try
+            {
+                _perMonitorDpiAware = GetCurrentProcessDpiAwareness() == ProcessDpiAwareness.Process_Per_Monitor_DPI_Aware;
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            if (_perMonitorDpiAware)
+            {
+                _hwndSource = (HwndSource)PresentationSource.FromVisual(this);
+                _wpfDpi = 96.0 * _hwndSource.CompositionTarget.TransformToDevice.M11;
+                _currentDpi = GetDpiForHwnd(_hwndSource.Handle);
+                _scaleFactor = _currentDpi.X / _wpfDpi;
+                Width = Width * _scaleFactor;
+                Height = Height * _scaleFactor;
+                UpdateLayoutTransform(_scaleFactor);
+                //UpdateLayoutTransform(1.0);
+            }
+        }
+
+        void UpdateLayoutTransform(double scaleFactor)
+        {
+            if (_perMonitorDpiAware)
+            {
+                var child = GetVisualChild(0);
+                if (_scaleFactor != 1.0)
+                {
+                    var dpiScale = new ScaleTransform(scaleFactor, scaleFactor);
+                    child.SetValue(Window.LayoutTransformProperty, dpiScale);
+                }
+                else
+                {
+                    child.SetValue(Window.LayoutTransformProperty, null);
+                }
+            }
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            HwndSource source = PresentationSource.FromVisual(this) as HwndSource;
+            source.AddHook(WndProc);
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            // Handle messages...
+            switch (msg)
+            {
+                case WM_DPICHANGED:
+
+                    RECT lprNewRect = (RECT)Marshal.PtrToStructure(lParam, typeof(RECT));
+
+                    var newWidth = lprNewRect.Right - lprNewRect.Left;
+                    var newHeight = lprNewRect.Bottom - lprNewRect.Top;
+
+                    Debug.WriteLine($"lprect width: {newWidth}, height: {newHeight}");
+
+                    SetWindowPos(
+                        hwnd,
+                        IntPtr.Zero,
+                        lprNewRect.Left,
+                        lprNewRect.Top,
+                        newWidth,
+                        newHeight,
+                        SWP_NOACTIVATE | SWP_NOOWNERZORDER | SWP_NOZORDER);
+                    var oldDpi = _currentDpi;
+                    _currentDpi = new Point
+                    {
+                        X = (double)(wParam.ToInt32() >> 16),
+                        Y = (double)(wParam.ToInt32() & 0x0000FFFF)
+                    };
+                    Debug.WriteLine($"current dpi: {_currentDpi}");
+                    Log.Info("DISPLAY", $"current dpi: {_currentDpi}");
+                    if (oldDpi.X != _currentDpi.X || oldDpi.Y != _currentDpi.Y)
+                    {
+                        Debug.WriteLine("DPI Changed!!");
+
+                        DpiChanged();
+
+                    }
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        Point GetDpiForHwnd(IntPtr hwnd)
+        {
+            IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+            uint newDpiX = 96;
+            uint newDpiY = 96;
+            if (GetDpiForMonitor(monitor, (int)MonitorDpiType.MDT_Effective_DPI, ref newDpiX, ref newDpiY) != 0)
+            {
+                return new Point(96.0, 96.0);
+            }
+            return new Point((double)newDpiX, (double)newDpiY);
+        }
+
+        ProcessDpiAwareness GetCurrentProcessDpiAwareness()
+        {
+            ProcessDpiAwareness awareness = ProcessDpiAwareness.Process_DPI_Unaware;
+            var res = GetProcessDpiAwareness(Process.GetCurrentProcess().Handle, ref awareness);
+            return awareness;
+        }
+
+        void DpiChanged()
+        {
+            _scaleFactor = _currentDpi.X / _wpfDpi;
+            Debug.WriteLine($"Scale factor is {_scaleFactor}");
+            UpdateLayoutTransform(_scaleFactor);
+            //UpdateLayoutTransform(1.0);
+            Debug.WriteLine($"width: {Width}, height: {Height}");
+        }
+    }
+}
---
+++ Utilizr.WPF/Validation/Validater.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Utilizr.Globalisation;
+
+namespace Utilizr.WPF.Validation
+{
+    public delegate ValidationResult CustomValidationDelegate(string input);
+
+    /// <summary>
+    /// Opportunity to tweak input before validation applied.
+    /// E.g. may wish to trim spaces at the start/end to save user having to do it
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public delegate string PreValidateCoercion(string input);
+
+
+    public class Validater
+    {
+        public EventHandler? ClearErrorRequest;
+        public EventHandler<ValidationResult>? Validated;
+
+        public CustomValidationDelegate ValidationDelegate { get; set; }
+        public PreValidateCoercion? PreValidateCoercion { get; set; }
+
+        public Validater(CustomValidationDelegate validationdelegate)
+        {
+            ValidationDelegate = validationdelegate;
+        }
+
+        public Validater(CustomValidationDelegate validationdelegate, PreValidateCoercion? preValidateCoercion)
+            : this(validationdelegate)
+        {
+            PreValidateCoercion = preValidateCoercion;
+        }
+
+        public ValidationResult Validate(string input)
+        {
+            var result = new ValidationResult
+            {
+                IsValid = true
+            };
+
+            if (ValidationDelegate == null)
+                return result;
+
+            if (PreValidateCoercion == null)
+            {
+                result.MergeResult(ValidationDelegate(input));
+            }
+            else
+            {
+                result.MergeResult(ValidationDelegate(PreValidateCoercion(input)));
+            }
+
+            OnValidationResult(result);
+            return result;
+        }
+
+        public void ClearErrors()
+        {
+            OnClearErrorRequest();
+        }
+
+        protected virtual void OnClearErrorRequest()
+        {
+            ClearErrorRequest?.Invoke(this, new EventArgs());
+        }
+
+        protected virtual void OnValidationResult(ValidationResult result)
+        {
+            Validated?.Invoke(this, result);
+        }
+
+        #region DefaultValidaters
+
+        public static Validater CreateEmailValidater(PreValidateCoercion? preValidateCoercion = null)
+        {
+            return new Validater(ValidateEmail, preValidateCoercion);
+        }
+
+        public static Validater CreateNameValidater(int minLength, PreValidateCoercion? preValidateCoercion = null)
+        {
+            return new Validater((input) => ValidateName(input, minLength), preValidateCoercion);
+        }
+
+        //TODO: expand check symbols / caps
+        public static Validater CreatePasswordValidater(int minLength, PreValidateCoercion? preValidateCoercion = null)
+        {
+            return new Validater((input) => ValidatePassword(input, minLength), preValidateCoercion);
+        }
+
+        public static Validater CreateIntegerValidater(int? minimum = null, int? maximum = null, PreValidateCoercion? preValidateCoercion = null)
+        {
+            return new Validater((input) => ValidateInteger(input, minimum, maximum), preValidateCoercion);
+        }
+
+        static ValidationResult ValidateEmail(string input)
+        {
+            var result = new ValidationResult() {IsValid = true};
+            if (string.IsNullOrEmpty(input))
+            {
+                result.IsValid = false;
+                result.Messages.Add(L._I("Please enter an email address"));
+                return result;
+            }
+
+            var regexPattern = @"^[_a-zA-Z0-9-+.!#$%&'*+-/=?^_`{|}~]+(\.[_a-zA-Z0-9-+]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)";
+            var regex = new Regex(regexPattern, RegexOptions.IgnoreCase);
+            if (!regex.IsMatch(input))
+            {
+                result.IsValid = false;
+                result.Messages.Add(L._I("Please enter a valid email address"));
+            }
+            return result;
+        }
+
+        static ValidationResult ValidateName(string input, int minLength)
+        {
+            return ValidateMinimumLength(
+                input,
+                minLength,
+                L._I("Please enter a name"), 
+                L._I("Please enter a name (minimum {0} characters)", () => L.Args(minLength)));
+        }
+
+        static ValidationResult ValidatePassword(string input, int minLength)
+        {
+            return ValidateMinimumLength(
+                input,
+                minLength,
+                L._I("Please enter a password"),
+                L._I("Please enter a password (minimum {0} characters)", () => L.Args(minLength)));
+        }
+
+        static ValidationResult ValidateMinimumLength(string input, int minLength, ITranslatable noInputMessage, ITranslatable underMinLengthMessage)
+        {
+            var result = new ValidationResult() { IsValid = true };
+            if (string.IsNullOrEmpty(input))
+            {
+                result.IsValid = false;
+                result.Messages.Add(noInputMessage);
+                return result;
+            }
+
+            if (input.Length < minLength)
+            {
+                result.IsValid = false;
+                result.Messages.Add(underMinLengthMessage);
+            }
+
+            return result;
+        }
+
+        static ValidationResult ValidateInteger(string input, int? minimum, int? maximum)
+        {
+            var result = new ValidationResult() { IsValid = true };
+            if (string.IsNullOrEmpty(input))
+            {
+                result.IsValid = false;
+                //## Data validation on user inputted text
+                result.Messages.Add(L._I("Please enter a number."));
+                return result;
+            }
+
+            // Better than using Char.IsDigit. This handles negative numbers, need to parse anyway if min / max specified...
+            if (!Int64.TryParse(input, out long parsedNumber))
+            {
+                result.IsValid = false;
+                result.Messages.Add(L._I("Please enter digits only."));
+                return result;
+            }
+
+            if (minimum.HasValue && parsedNumber < minimum.Value)
+            {
+                result.IsValid = false;
+                result.Messages.Add(L._I("Please enter a number equal to or greater than {0}.", () => L.Args(minimum.Value)));
+                return result;
+            }
+
+            if (maximum.HasValue && parsedNumber > maximum.Value)
+            {
+                result.IsValid = false;
+                result.Messages.Add(L._I("Please enter a number equal to or less than {0}.", () => L.Args(maximum.Value)));
+                return result;
+            }
+
+            return result;
+        }
+#endregion
+    }
+}
