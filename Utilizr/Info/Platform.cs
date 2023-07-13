377d992622f2ff1ce646cb6e4dbb5bd857837989
Darren Hale <djhale.development@gmail.com>
2023-07-13 13:11:21 +0100

    Added WinPlatform which requires windows specific dependencies. Includes OSVersionHumar and Ram properties.

--- Utilizr/Info/Platform.cs
+++ Utilizr/Info/Platform.cs
@@ -95,52 +95,6 @@ namespace Utilizr.Info
                 return osVersion;
             }
         }
-
-        ///// <summary>
-        ///// Human readable version of OSVersion. E.g. Microsoft Windows 10 Pro
-        ///// </summary>
-        //public static string OSVersionHuman
-        //{
-        //    get
-        //    {
-        //        string? windowsVer = null;
-        //        try
-        //        {
-        //            var key = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows NT\CurrentVersion");
-        //            windowsVer = key?.GetValue("ProductName", string.Empty) as string;
-        //        }
-        //        catch { /* swallow */ }
-
-        //        if (!string.IsNullOrEmpty(windowsVer))
-        //            return windowsVer;
-
-        //        return Environment.OSVersion.VersionString;
-        //    }
-        //}
-
-
-        //private static ulong? _ram;
-        ///// <summary>
-        ///// Total amount of RAM installed on the computer, not how much is in use.
-        ///// </summary>
-        //public static ulong Ram
-        //{
-        //    get
-        //    {
-        //        if (!_ram.HasValue)
-        //        {
-        //            const string capcity = "Capacity";
-        //            var query = $"SELECT {capcity} FROM Win32_PhysicalMemory";
-        //            var searcher = new ManagementObjectSearcher(query);
-        //            foreach (var wmiPart in searcher.Get())
-        //            {
-        //                _ram = Convert.ToUInt64(wmiPart.Properties[capcity].Value);
-        //                break;//}
-        //            }
-        //        }
-        //        return _ram ?? 0UL;
-        //    }
-        
     }
 
 
