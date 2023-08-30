81dc49d75322e39a652e1e7994466697f437c229
krzysztof.zajac <krzysztof.zajac@protected.net>
2023-08-30 09:49:09 +0100

    #UriToImageFallbackConverter New converter added.

---
+++ Utilizr.WPF/Converters/UriToImageFallbackConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Utilizr.WPF.Util;
+
+namespace Utilizr.WPF.Converters
+{
+    public class UriToImageFallbackConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var fullFilePath = (string)value;
+
+            if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable() == true)
+                return ResourceHelper.GetImageSource("logo-grey-error@2x.png");
+            
+            BitmapImage bitmap = new BitmapImage();
+            try
+            {
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(fullFilePath, UriKind.Absolute);
+                bitmap.EndInit();
+            } 
+            catch 
+            {
+                return ResourceHelper.GetImageSource("logo-grey-error@2x.png");
+            }
+
+            return bitmap;
+        }
+        
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
