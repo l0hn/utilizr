1ef27f113c58f18e191c1bfe1a1bfdc5009e79ca
Darren Hale <djhale.development@gmail.com>
2023-09-29 14:09:25 +0100

    Toggled control updated to handle different sizes. Needs more work as BackgroundOn and BackgroundOff don't seem to fire when changes from styles...although it now should work, creating new storyboards. Added ARGBToColour() string extension method.

---
+++ Utilizr.WPF/Extension/StringEx.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Media;
+
+namespace Utilizr.WPF.Extension
+{
+    public static class StringEx
+    {
+        /// <summary>
+        /// E.g. #FF000000
+        /// </summary>
+        /// <param name="hex"></param>
+        /// <returns></returns>
+        public static Color ARGBToColor(this string hex)
+        {
+            return (Color)ColorConverter.ConvertFromString(hex);
+        }
+    }
+}
