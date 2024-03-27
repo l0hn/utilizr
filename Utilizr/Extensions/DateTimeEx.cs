c23bce9212ba780cfe4003a58da5f26d31338998
Darren Hale <djhale.development@gmail.com>
2024-03-27 16:02:21 +0000

    Added more DateTime extension methods: GetNextDay() and SetTime().

--- Utilizr/Extensions/DateTimeEx.cs
+++ Utilizr/Extensions/DateTimeEx.cs
@@ -91,5 +91,65 @@ namespace Utilizr.Extensions
             // todo: DateTimeEx.TotalYears() may be a bit off for cultures not using Gregorian calendar
             return (zeroTime + timeSpan).Year - 1;
         }
+
+        /// <summary>
+        /// Return a DateTime instance matching the next (or previous) specified day.
+        /// </summary>
+        /// <param name="dateTime">Current date and time.</param>
+        /// <param name="dayToFind">The desired day to find based based on the <see cref="dateTime"/></param> value.
+        /// <param name="futureDate">Whether we're finding the day in the future, or in the past.</param>
+        /// <returns></returns>
+        public static DateTime GetNextDay(this DateTime dateTime, DayOfWeek dayToFind, bool futureDate)
+        {
+            return GetNextDay(dateTime, dayToFind, futureDate, out _);
+        }
+
+        /// <summary>
+        /// Return a DateTime instance matching the next (or previous) specified day.
+        /// </summary>
+        /// <param name="dateTime">Current date and time.</param>
+        /// <param name="dayToFind">The desired day to find based based on the <see cref="dateTime"/></param> value.
+        /// <param name="futureDate">Whether we're finding the day in the future, or in the past.</param>
+        /// <param name="alreadyMatched">Whether any offset was applied due to the day already matching.</param>
+        /// <returns></returns>
+        public static DateTime GetNextDay(this DateTime dateTime, DayOfWeek dayToFind, bool futureDate, out bool alreadyMatched)
+        {
+            var dayDelta = CalculateDayOffset(dateTime.DayOfWeek, dayToFind);
+            alreadyMatched = dayDelta == 0;
+            return dateTime.AddDays(futureDate ? dayDelta : (0 - dayDelta));
+        }
+
+        /// <summary>
+        /// Calculate the number of days between the given two values.
+        /// </summary>
+        /// <param name="currentDay"></param>
+        /// <param name="dayOfDesiredWeek"></param>
+        /// <returns></returns>
+        public static int CalculateDayOffset(DayOfWeek currentDay, DayOfWeek dayOfDesiredWeek)
+        {
+            int c = (int)currentDay;
+            int d = (int)dayOfDesiredWeek;
+            return (7 - c + d) % 7;
+        }
+
+        /// <summary>
+        /// Create a DateTime with the same Date, but set the time with the given values.
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="hours"></param>
+        /// <param name="minutes">Optional minutes, defaults to 0.</param>
+        /// <param name="seconds">Optional seconds, defaults to 0.</param>
+        /// <returns></returns>
+        public static DateTime SetTime(this DateTime dt, int hours, int? minutes = null, int? seconds = null)
+        {
+            return new DateTime(
+                dt.Year,
+                dt.Month,
+                dt.Day,
+                hours,
+                minutes ?? 0,
+                seconds ?? 0
+            );
+        }
     }
 }
