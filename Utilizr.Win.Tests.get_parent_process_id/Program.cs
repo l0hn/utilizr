6a119998759e6fda5bc955468de18d58a0682119
John Haines <jahhai@gmail.com>
2024-11-17 14:39:04 +0000

    WIP get parent process test

---
+++ Utilizr.Win.Tests.get_parent_process_id/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Diagnostics;
+using Utilizr.Win.Extensions;
+
+
+if (Process.GetCurrentProcess().TryGetParentProcess(out var parentProcess) && parentProcess != null)
+{
+    Console.WriteLine(parentProcess.Id);
+    Environment.Exit(parentProcess.Id);
+}
+
+Environment.Exit(-1);
---
+++ Utilizr.Win.Tests/ProcessTests/ProcessTests.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using NUnit.Framework;
+using Utilizr;
+using Utilizr.Win.Extensions;
+
+namespace Tests
+{
+    public class ProcessTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            //build the console app
+            int result = Shell.Exec("dotnet", "build", "../../../../Utilizr.Win.Tests.get_parent_process_id/get_parent_process_id.csproj").ExitCode;
+            if (result != 0)
+            {
+                throw new Exception("failed to build console project");
+            }
+        }
+
+        [Test]
+        public void GetParentProcess()
+        {
+            Console.WriteLine($"base dir: {AppContext.BaseDirectory}");            
+            var thisProcess = Process.GetCurrentProcess();
+            var thisPid = thisProcess.Id;
+            var resultFromSubProcess = Shell.Exec("../../../../Utilizr.Win.Tests.get_parent_process_id/bin/Debug/net8-windows/get_parent_process_id.exe");
+
+            Assert.That(resultFromSubProcess.ExitCode, Is.EqualTo(thisPid));
+        }
+    }
+}
