8cc8fc4786a417b0df044ec5266375bcbfa8e7fd
Darren Hale <djhale.development@gmail.com>
2025-06-27 10:17:23 +0100

    Added RandomEx for an extension method, GenreateAlphanumericString(). Can specify the length of a random string to generate, including upper and lower case.

---
+++ Utilizr/Extensions/RandomEx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Utilizr.Extensions
+{
+    public static class RandomEx
+    {
+        public static string GenreateAlphanumericString(this Random random, int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                                        .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
