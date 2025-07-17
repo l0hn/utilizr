7cb416014ab480e991c66a4958d2c3514e430f33
Darren Hale <djhale.development@gmail.com>
2025-07-17 13:39:23 +0100

    Added VersionEx with SafeToString(int fieldCount) extension method. Adds checks so the specified fieldCount can never be greater than the specified fields.

---
+++ Utilizr/Extensions/VersionEx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Utilizr.Extensions
+{
+    public static class VersionEx
+    {
+        public static string SafeToString(this Version version, int fieldCount)
+        {
+            var safeFieldCount = Math.Min(fieldCount, GetDefinedFieldCount(version));
+            return version.ToString(safeFieldCount);
+        }
+
+        /// <summary>
+        /// Check against the various components to ensure a valid fieldCount.
+        /// </summary>
+        /// <param name="version"></param>
+        /// <returns></returns>
+        public static int GetDefinedFieldCount(this Version? version)
+        {
+            if (version == null)
+                return 0;
+
+            if (version.Revision >= 0)
+                return 4;
+
+            if (version.Build >= 0)
+                return 3;
+
+            return 2;
+        }
+    }
+}
