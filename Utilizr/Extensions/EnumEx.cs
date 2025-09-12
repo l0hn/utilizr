f7e178e858e7638f95f6f004c38aaad84d6f1a5b
Darren Hale <djhale.development@gmail.com>
2025-09-12 16:52:18 +0100

    Added EnumEx with the ability to get the enum's name in lower snake case.

---
+++ Utilizr/Extensions/EnumEx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Utilizr.Extensions
+{
+    public static class EnumEx
+    {
+        public static string ToLowerSnakeCase(this Enum value)
+        {
+            string name = value.ToString();
+            string snake = Regex.Replace(name, @"(?<!^)([A-Z])", "_$1");
+            return snake.ToLowerInvariant();
+        }
+    }
+}
