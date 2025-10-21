b3f8c4193440045acea83f19d024002830c90622
Darren Hale <djhale.development@gmail.com>
2025-10-21 10:11:54 +0100

    Added L.CurrentLanguageCulture which attempts to match-up from L.CurrentLanguage.

--- Utilizr.Globalisation/L.cs
+++ Utilizr.Globalisation/L.cs
@@ -27,17 +27,69 @@ namespace Utilizr.Globalisation
 
         private static bool _indexedMoFiles = false;
         public const string LogCat = "Utilizr.Globalisation";
+        const string _debugLanguageTag = "blank";
 
         public static string CurrentLanguage { get; private set; }
 #if DEBUG
         public static SupportedLanguage DebugLanguage { get; }
 #endif
 
+        private static string? _cachedLanguageCheck;
+        private static CultureInfo? _cachedCultureInfo;
+        /// <summary>
+        /// Attempts to return the matching .NET CultureInfo from <see cref="CurrentLanguage"/> and remains 
+        /// cached until the language has changed.
+        /// 
+        /// If a neutral language is used, such as 'en', it will check the current thread's culture to see
+        /// if it can return the specific region, such as 'en-US' or 'en-GB'.
+        /// 
+        /// Falls back to the thread's culture if no match has been found.
+        /// </summary>
+        public static CultureInfo CurrentLanguageCultureInfo
+        {
+            get
+            {
+                var currentLanguage = CurrentLanguage;
+                if (_cachedLanguageCheck == currentLanguage && _cachedCultureInfo != null)
+                    return _cachedCultureInfo; // can't use _cachedCultureInfo.Name due to debug language
+
+                if (currentLanguage != _debugLanguageTag)
+                {
+                    var searchTag = currentLanguage;
+                    if (!searchTag.Contains("-"))
+                    {
+                        var usersMachine = Thread.CurrentThread.CurrentCulture.Name;
+                        if (usersMachine.StartsWith($"{searchTag}-"))
+                            searchTag = usersMachine; // matched neutral, user machine's specific culture
+                    }
+
+                    var cultureType = searchTag.Contains("-")
+                        ? CultureTypes.SpecificCultures
+                        : CultureTypes.NeutralCultures;
+
+                    var match = CultureInfo.GetCultures(cultureType)
+                        .FirstOrDefault(p => string.Equals(p.Name, searchTag, StringComparison.OrdinalIgnoreCase));
+
+                    if (match != null)
+                    {
+                        _cachedCultureInfo = match;
+                        _cachedLanguageCheck = currentLanguage;
+                        return _cachedCultureInfo;
+                    }
+                }
+
+                // fallback to UI thread language
+                _cachedCultureInfo = Thread.CurrentThread.CurrentCulture;
+                _cachedLanguageCheck = currentLanguage;
+                return _cachedCultureInfo;
+            }
+        }
+
         static L()
         {
             CurrentLanguage = "en-GB"; // placeholder
 #if DEBUG
-            DebugLanguage = new SupportedLanguage(name: "Blank", nativeName: "*****", ietfLanguageTag: "blank");
+            DebugLanguage = new SupportedLanguage(name: "Blank", nativeName: "*****", ietfLanguageTag: _debugLanguageTag);
 #endif
         }
 
