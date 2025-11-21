271423e092168eac8457b6c8e88d7cc5fa0c9a1e
Darren Hale <djhale.development@gmail.com>
2025-11-21 16:55:33 +0000

    Added three new attached properties. BorderBehaviours.AutoRoundCorners based on ActualHeight. UIElementFocusRestoreBehaviour to return the focus based on an 'is busy' boolean binding. UIElementKeyDownBehaviour to binding command (and parameter) for specific keys on arbitrary controls.

---
+++ Utilizr.WPF/Attached/UIElementFocusRestoreBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace Utilizr.WPF.Attached
+{
+    /// <summary>
+    /// The intention here is to restore focus to the UIElement when an action has completed.
+    /// E.g. UIElement is disabled while waiting for an async action, restore the focus when it completes.
+    /// </summary>
+    public static class UIElementFocusRestoreBehaviour
+    {
+        public static readonly DependencyProperty TrackFocusForRestoreProperty =
+            DependencyProperty.RegisterAttached(
+                "TrackFocusForRestore",
+                typeof(bool),
+                typeof(UIElementFocusRestoreBehaviour),
+                new PropertyMetadata(false, OnTrackFocusForRestoreChanged));
+
+        public static void SetTrackFocusForRestore(DependencyObject element, bool value)
+        {
+            element.SetValue(TrackFocusForRestoreProperty, value);
+        }
+
+        public static bool GetTrackFocusForRestore(DependencyObject element)
+        {
+            return (bool)element.GetValue(TrackFocusForRestoreProperty);
+        }
+
+        private static void OnTrackFocusForRestoreChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is UIElement uiElement && !(bool)e.NewValue)
+            {
+                uiElement.Focus();
+            }
+        }
+    }
+}
---
+++ Utilizr.WPF/Attached/UIElementKeyDownBehaviour.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Utilizr.WPF.Attached
+{
+    public static class UIElementKeyDownBehavior
+    {
+        public static readonly DependencyProperty KeysProperty =
+            DependencyProperty.RegisterAttached(
+                "Keys",
+                typeof(IEnumerable<Key>),
+                typeof(UIElementKeyDownBehavior),
+                new PropertyMetadata(new List<Key> { Key.Enter })
+            );
+
+        public static void SetKeys(DependencyObject element, IEnumerable<Key> value)
+        {
+            element.SetValue(KeysProperty, value);
+        }
+
+        public static IEnumerable<Key> GetKeys(DependencyObject element)
+        {
+            return (IEnumerable<Key>)element.GetValue(KeysProperty);
+        }
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.RegisterAttached(
+                "Command",
+                typeof(ICommand),
+                typeof(UIElementKeyDownBehavior),
+                new PropertyMetadata(null, OnCommandChanged)
+            );
+
+        public static void SetCommand(DependencyObject element, ICommand value)
+        {
+            element.SetValue(CommandProperty, value);
+        }
+
+        public static ICommand GetCommand(DependencyObject element)
+        {
+            return (ICommand)element.GetValue(CommandProperty);
+        }
+
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.RegisterAttached(
+                "CommandParameter",
+                typeof(object),
+                typeof(UIElementKeyDownBehavior),
+                new PropertyMetadata(null));
+
+        public static void SetCommandParameter(DependencyObject element, object value)
+        {
+            element.SetValue(CommandParameterProperty, value);
+        }
+
+        public static object GetCommandParameter(DependencyObject element)
+        {
+            return element.GetValue(CommandParameterProperty);
+        }
+
+        private static void OnCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is UIElement uiElement)
+            {
+                if (e.NewValue != null)
+                {
+                    uiElement.PreviewKeyDown += UiElement_PreviewKeyDown;
+                }
+                else
+                {
+                    uiElement.PreviewKeyDown -= UiElement_PreviewKeyDown;
+                }
+            }
+        }
+
+        private static void UiElement_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (!(sender is UIElement uiElement))
+                return;
+
+            var keys = GetKeys(uiElement);
+            var command = GetCommand(uiElement);
+            var parameter = GetCommandParameter(uiElement);
+
+            if (keys != null && command != null)
+            {
+                foreach (var key in keys)
+                {
+                    if (e.Key == key)
+                    {
+                        if (command.CanExecute(parameter))
+                        {
+                            command.Execute(parameter);
+                            e.Handled = true;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
