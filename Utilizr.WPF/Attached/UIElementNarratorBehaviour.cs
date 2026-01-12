1c22980a6bbce1e7cd2255a22bcb67d66d563c58
Darren Hale <djhale.development@gmail.com>
2026-01-12 11:46:02 +0000

    Added UIElementNarratorBehaviour.cs.

---
+++ Utilizr.WPF/Attached/UIElementNarratorBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Automation;
+
+namespace Utilizr.WPF.Attached
+{
+    public static class UIElementNarratorBehaviour
+    {
+        public static readonly DependencyProperty NarratorDescriptionProperty =
+            DependencyProperty.RegisterAttached(
+                "NarratorDescription",
+                typeof(string),
+                typeof(UIElementNarratorBehaviour),
+                new PropertyMetadata(string.Empty, OnNarratorDescriptionChanged)
+            );
+
+        public static string GetNarratorDescription(DependencyObject obj)
+        {
+            return (string)obj.GetValue(NarratorDescriptionProperty);
+        }
+
+        public static void SetNarratorDescription(DependencyObject obj, string value)
+        {
+            obj.SetValue(NarratorDescriptionProperty, value);
+        }
+
+        private static void OnNarratorDescriptionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is UIElement element && e.NewValue is string narratorDescription)
+            {
+                AutomationProperties.SetName(element, narratorDescription);
+                AutomationProperties.SetHelpText(element, narratorDescription);
+            }
+        }
+    }
+}
